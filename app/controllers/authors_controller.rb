519848f0c34f8e0fef81582fea471c889425089c
Brandon Weaver <a7xncob@gmail.com>
2020-01-24 21:54:56 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -10,6 +10,7 @@ class AuthorsController < ApplicationController
     @author = Author.new(author_params)
     
     if @author.validate
+      @author.save
       redirect_to author_path(@author)
     else
       render "new"
